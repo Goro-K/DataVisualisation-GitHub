f9bd7dc2d0809cf7eed4f7603e0f61c867b3d2c7
Goro-K <romankizilt@gmail.com>
2024-09-08 13:24:26 +0200

    title animation

---
+++ app/components/GitHubTitleAnimated.tsx
@@ -0,0 +1,12 @@
+import "./github-title-animated.css";
+
+const GitHubTitleAnimated: React.FC = () => {
+  return (
+    <h1 className="title">
+      <span className="title_frame_1">Data Visualisation</span>
+      <span className="title_frame_2"> GitHub</span>
+    </h1>
+  );
+};
+
+export default GitHubTitleAnimated;
